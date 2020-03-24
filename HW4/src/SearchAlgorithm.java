802cf86e6e85c2f9496b113a228559e1ef08f719
Victor Vargas <vevargascor@miners.utep.edu>
2020-03-24 16:00:45 -0600

    Started simmulated annealing, not tested

--- HW4/src/SearchAlgorithm.java
+++ HW4/src/SearchAlgorithm.java
@@ -1,5 +1,84 @@
 public class SearchAlgorithm {
 
+  public Schedule simulatedAnnealing(SchedulingProblem problem, long deadline){
+    //Empty schedule to fill
+    Schedule solution = problem.getEmptySchedule();
+    //Schedules that will be used for the annealing process.
+    Schedule current = solution;
+    Schedule next = solution;
+    //Temperature value that will help simulate the annealing.
+    double T = 1;
+    //Limit to the annealing
+    long limit = 5000;
+    //Initial Solution
+    current = randomSolution(current,problem);
+    //Change in score
+    double scoreDelta = problem.evaluateSchedule(next)-problem.evaluateSchedule(current);
+
+    for(;T<limit;T++){
+      next = randomSuccessor(current,problem);
+      if(scoreDelta>0){
+        current = next;
+      }else{
+        if(Math.exp(scoreDelta/T)>Math.random()){
+          current = next;
+        }
+      }
+    }
+
+    return solution;
+  }
+
+  /**
+   * Method that will generate a neighbor from the current solution given, by either randomly permutating the solution, or
+   * modifying a few of the assignments.
+   * @param current
+   * @param problem
+   * @return
+   */
+  private Schedule randomSuccessor(Schedule current, SchedulingProblem problem) {
+    return null;
+  }
+
+  /**
+   * Method that provides an initial random solution to start with. Will be
+   * randomly generated but still follow the restrictions and constraints, so it
+   * will be a valid solution.
+   * 
+   * @param current
+   * @param problem
+   * @return
+   */
+  private Schedule randomSolution(Schedule current, SchedulingProblem problem) {
+    
+    
+    return null;
+  }
+
+  public Schedule backtrackSearch(SchedulingProblem problem, long deadline) {
+    //Empty schedule to fill
+    Schedule solution = problem.getEmptySchedule();
+    return backtrackRecursive(solution, problem, deadline);
+  }
+  
+  /**
+   * Function that performs recursive backtracking.
+   * @param solution
+   * @param problem
+   * @param deadline
+   * @return
+   */
+  public Schedule backtrackRecursive(Schedule solution,SchedulingProblem problem, long deadline){
+    return  solution;
+  }
+
+
+  /**
+   * 
+   * @param problem
+   * @param deadline
+   * @return
+   */
   // Your search algorithm should return a solution in the form of a valid
   // schedule before the deadline given (deadline is given by system time in ms)
   public Schedule solve(SchedulingProblem problem, long deadline) {
